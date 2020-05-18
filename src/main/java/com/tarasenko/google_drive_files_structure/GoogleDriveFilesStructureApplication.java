8fa8b1316bf2833b329dc74314278e5db35792d8
Andrey Tarasenko <1997wilred@gmail.com>
2020-05-18 04:51:30 +0300

    my first commit

---
+++ src/main/java/com/tarasenko/google_drive_files_structure/GoogleDriveFilesStructureApplication.java
@@ -0,0 +1,13 @@
+package com.tarasenko.google_drive_files_structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDriveFilesStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleDriveFilesStructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tarasenko/google_drive_files_structure/GoogleDriveFilesStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarasenko.google_drive_files_structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleDriveFilesStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
